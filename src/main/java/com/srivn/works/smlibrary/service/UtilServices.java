aad1a1b2353947d4cb30921cf467000306bcd953
dev02-srivnworks <dev02.srivnworks@gmail.com>
2023-03-21 15:33:25 +0000

    sample testing sonarqube

--- src/main/java/com/srivn/works/smlibrary/service/UtilServices.java
+++ src/main/java/com/srivn/works/smlibrary/service/UtilServices.java
@@ -67,7 +67,7 @@ public class UtilServices {
 		if (enList != null) {
 			return enList;
 		} else {
-			throw new SMException(AppMsg.Err.ERR_001.getCode(), AppMsg.Err.ERR_001.getMsgWithParam(clsnVal));
+			throw new SMException(AppMsg.Err.ERR_001.getCode(), AppMsg.Err.ERR_001.getMsgWithParam("Clsn List"));
 		}
 	} catch (Exception e) {
 		throw new SMException(AppMsg.Err.ERR_000.getCode(), AppMsg.Err.ERR_000.getMsgWithParam());
